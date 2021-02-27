8267466ff67438368ee04e635bc4acc9712178fe
HOuadhour <HOuadhour@Yandex.com>
2021-02-27 12:01:04 +0100

    Finishing the project and deploying to heroku

---
+++ README.md
@@ -0,0 +1,5 @@
+# Request Header Parser Microservice
+
+Second Project of the 5th curriculum APIs and Microservices
+
+[Heroku Live App](https://afternoon-coast-94396.herokuapp.com/)
---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+app.get("/api/whoami", (req, res) => {
+  const lang = req.headers["accept-language"];
+  const ip = req.headers.host.split(":")[0];
+  const soft = req.headers["user-agent"];
+
+  res.json({
+    ipaddress: ip,
+    language: lang,
+    software: soft,
+  });
+});
+
+module.exports = app;
